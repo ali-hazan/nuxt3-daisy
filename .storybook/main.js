44d8b289eedde16fcb3673a91f70085ce945feea
Ali Hassan <alihassankp27@gmail.com>
2023-03-13 23:21:08 +0400

    feature: integrate tailwind and daisy ui to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,18 @@ export default {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    }
   ],
   "framework": {
     "name": "@storybook/vue3-vite",
